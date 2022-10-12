002a43aa76752ef0fd3a6553081c95697d378359
aaaa091 <st876504@gmail.com>
2022-10-12 11:33:46 +0800

    加入專案檔案。

---
+++ cs-posSystem/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace cs_posSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_input = new System.Windows.Forms.Button();
+            this.btn_output = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbox_price = new System.Windows.Forms.TextBox();
+            this.tbox_amount = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbtn_purchase = new System.Windows.Forms.RadioButton();
+            this.rbtn_sale = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn_input
+            // 
+            this.btn_input.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn_input.Location = new System.Drawing.Point(55, 399);
+            this.btn_input.Name = "btn_input";
+            this.btn_input.Size = new System.Drawing.Size(126, 37);
+            this.btn_input.TabIndex = 0;
+            this.btn_input.Text = "輸入資料";
+            this.btn_input.UseVisualStyleBackColor = true;
+            this.btn_input.Click += new System.EventHandler(this.btn_input_Click);
+            // 
+            // btn_output
+            // 
+            this.btn_output.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn_output.Location = new System.Drawing.Point(187, 399);
+            this.btn_output.Name = "btn_output";
+            this.btn_output.Size = new System.Drawing.Size(126, 37);
+            this.btn_output.TabIndex = 1;
+            this.btn_output.Text = "輸出資料";
+            this.btn_output.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(50, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 27);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "編號";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(50, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 27);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "貨品";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(50, 174);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 27);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "單價";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.Location = new System.Drawing.Point(50, 228);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 27);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "數量";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label5.Location = new System.Drawing.Point(182, 63);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 27);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "--";
+            // 
+            // tbox_price
+            // 
+            this.tbox_price.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.tbox_price.Location = new System.Drawing.Point(123, 173);
+            this.tbox_price.Name = "tbox_price";
+            this.tbox_price.Size = new System.Drawing.Size(190, 33);
+            this.tbox_price.TabIndex = 7;
+            // 
+            // tbox_amount
+            // 
+            this.tbox_amount.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.tbox_amount.Location = new System.Drawing.Point(123, 225);
+            this.tbox_amount.Name = "tbox_amount";
+            this.tbox_amount.Size = new System.Drawing.Size(190, 35);
+            this.tbox_amount.TabIndex = 8;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(55, 334);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(258, 59);
+            this.richTextBox1.TabIndex = 9;
+            this.richTextBox1.Text = "";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbtn_sale);
+            this.groupBox1.Controls.Add(this.rbtn_purchase);
+            this.groupBox1.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.groupBox1.Location = new System.Drawing.Point(55, 266);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(258, 58);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "進出貨";
+            // 
+            // rbtn_purchase
+            // 
+            this.rbtn_purchase.AutoSize = true;
+            this.rbtn_purchase.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.rbtn_purchase.Location = new System.Drawing.Point(7, 22);
+            this.rbtn_purchase.Name = "rbtn_purchase";
+            this.rbtn_purchase.Size = new System.Drawing.Size(72, 31);
+            this.rbtn_purchase.TabIndex = 0;
+            this.rbtn_purchase.TabStop = true;
+            this.rbtn_purchase.Text = "進貨";
+            this.rbtn_purchase.UseVisualStyleBackColor = true;
+            // 
+            // rbtn_sale
+            // 
+            this.rbtn_sale.AutoSize = true;
+            this.rbtn_sale.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.rbtn_sale.Location = new System.Drawing.Point(132, 21);
+            this.rbtn_sale.Name = "rbtn_sale";
+            this.rbtn_sale.Size = new System.Drawing.Size(72, 31);
+            this.rbtn_sale.TabIndex = 1;
+            this.rbtn_sale.TabStop = true;
+            this.rbtn_sale.Text = "出貨";
+            this.rbtn_sale.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(869, 471);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.tbox_amount);
+            this.Controls.Add(this.tbox_price);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_output);
+            this.Controls.Add(this.btn_input);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_input;
+        private System.Windows.Forms.Button btn_output;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbox_price;
+        private System.Windows.Forms.TextBox tbox_amount;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbtn_sale;
+        private System.Windows.Forms.RadioButton rbtn_purchase;
+    }
+}
+
---
+++ cs-posSystem/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cs_posSystem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_input_Click(object sender, EventArgs e)
+        {
+            string price = tbox_price.Text;
+            string amount = tbox_amount.Text;
+            string _type = "";
+            double _price = Convert.ToDouble(price);
+            double _amount = Convert.ToDouble(amount);
+            double _sum = _price * _amount;
+            string _sum_str = _sum.ToString();
+
+            if (rbtn_purchase.Checked)
+            {
+                _type = "進貨";
+            } else
+            {
+                _type = "出貨";
+            }
+
+            richTextBox1.Text = $"{_sum_str} {_type}";
+        }
+    }
+}
